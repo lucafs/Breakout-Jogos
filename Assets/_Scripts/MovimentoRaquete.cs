f653f5181514c53ae4c8493a195aeb307d325f7e
luca farah <luca@ambientedecriacao.com.br>
2021-03-03 08:52:49 -0300

    final

---
+++ Assets/_Scripts/MovimentoRaquete.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoRaquete : MonoBehaviour
+{
+    [Range(1, 10)]
+    public float velocidade;
+    GameManager gm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = GameManager.GetInstance();
+    }
+
+        // Update is called once per frame
+    void Update()
+    {
+    if(Input.GetKeyDown(KeyCode.Escape)) {
+            if(gm.gameState == GameManager.GameState.GAME){
+                gm.ChangeState(GameManager.GameState.PAUSE);
+            }
+            else if(gm.gameState == GameManager.GameState.PAUSE){
+                gm.ChangeState(GameManager.GameState.GAME);
+            }
+        }
+    if (gm.gameState != GameManager.GameState.GAME) return;
+
+    float inputX = Input.GetAxis("Horizontal");
+    transform.position += new Vector3(inputX, 0, 0) * Time.deltaTime * velocidade;  
+    }
+}
---
+++ Assets/_Scripts/UI_FimDeJogo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_FimDeJogo : MonoBehaviour
+{
+   public Text message;
+
+    GameManager gm;
+
+    public void Voltar()
+    {
+    gm.ChangeState(GameManager.GameState.GAME);
+    }
+   private void OnEnable()
+   {
+       gm = GameManager.GetInstance();
+
+       if(gm.vidas > 0)
+       {
+           message.text = "You won!!!";
+       }
+       else
+       {
+           message.text = "You lost!!!";
+       }
+   }
+}
